3cfabfac6aec2c7af5de56214864c8ad088bef18
reemhassan21 <reemhassan160299@gmail.com>
2022-09-30 09:52:46 +0200

    all my changes

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -4,22 +4,19 @@ import { EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-first',
   templateUrl: './first.component.html',
-  styleUrls: ['./first.component.css']
+  styleUrls: ['./first.component.css'],
 })
 export class FirstComponent {
-
-  name ="";
-  age=0
-  arr:{Name:string, Age:number}[]= []
+  name = '';
+  age = 0;
+  arr: { Name: string; Age: number }[] = [];
   @Output() myEvent = new EventEmitter();
 
-  Add(){
-    let data = {Name:this.name, Age:this.age}
-    if(this.age>20 && this.age<40)
-    {
-      this.arr.push(data)
+  Add() {
+    let data = { Name: this.name, Age: this.age };
+    if (this.age > 19 && this.age <= 40) {
+      this.arr.push(data);
       this.myEvent.emit(this.arr);
     }
   }
-
 }
